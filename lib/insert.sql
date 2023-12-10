2630b28a00cfc3eeefb3388e49d11fbf9efd658e
linder Opondo <achielinder@gmail.com>
2023-12-10 15:10:05 +0300

    completed assignments

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES 
+("Mr. Chocolate", 10, "M", "white", "choleric", 1),
+("Rowdy", 8, "M", "gray", "sanguine", 1),
+("Tabitha", 11, "W", "black", "melancholy", 1),
+("Sergeant Brown", 6, "M", "brown", "phlegmatic", 1),
+("Melissa", 14, "W", "black", "choleric", 1),
+("Grinch", 7, "M", "white", "sanguine", 1),
+("Wendy", 2, "W", "brown", "melancholy", 1),
+(NULL, 4, "M", "gray", "phlegmatic", 1);
